f6b253cf26e33f4f31758ea921fe194994c56c6f
ESSAMAMI <hamza.essamami.sio@gmail.com>
2018-05-01 18:03:54 +0200

    version 1.1

---
+++ src/PPE3/GsbFraisBundle/Controller/RechercherController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Electro Depot
+ * Date: 01/05/2018
+ * Time: 13:55
+ */
+
+namespace PPE3\GsbFraisBundle\Controller;
+
+
+class RechercherController
+{
+
+}
